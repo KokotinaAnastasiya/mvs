5f179f0f17f29b2961be444569618a54f788cedf
KokotinaAnastasiya <113551474+KokotinaAnastasiya@users.noreply.github.com>
2023-10-12 22:40:19 +0300

    Add files via upload

---
+++ src/main/java/netology /repository/PostRepositoryImpl.java
@@ -0,0 +1,50 @@
+package netology.repository;
+
+import netology.exception.NotFoundException;
+import netology.model.Post;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Repository
+public class PostRepositoryImpl implements PostRepository {
+    private final ConcurrentHashMap<Long, Post> posts;
+    private final AtomicLong idCounter = new AtomicLong(0L);
+
+    public PostRepositoryImpl(){
+        posts = new ConcurrentHashMap<>();
+    }
+    public List<Post> all() {
+        return new ArrayList<>(posts.values());
+    }
+
+    public Optional<Post> getById(long id) {
+        return Optional.ofNullable(posts.get(id));
+    }
+    public Post save(Post post) {
+        if (post.getId() != 0 && !posts.containsKey(post.getId())){
+            throw new NotFoundException();
+        }else {
+            posts.put(post.getId(), post);
+        }
+
+        if (post.getId() == 0){
+            long newId = idCounter.incrementAndGet();
+            post.setId(newId);
+            posts.put(post.getId(), post);
+        }
+        return post;
+    }
+
+    public void removeById(long id) {
+        if (posts.containsKey(id)){
+            posts.remove(id);
+        }else {
+            throw new NotFoundException("Вы ввели не верный id");
+        }
+    }
+}
